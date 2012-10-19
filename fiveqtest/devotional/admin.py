323ce8152c05aa7bd4d48162d24f3ac86f12b118
Horacio Duran <hduran@machinalis.com>
2012-10-19 10:42:40 -0300

    Added basic model and admin pages. Enabled admin and devotional apps in settings and urls for admin

---
+++ fiveqtest/devotional/admin.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 *-*
+from django.contrib import admin
+from devotional.models import Devotional
+
+admin.site.register(Devotional)
--- fiveqtest/devotional/models.py
+++ fiveqtest/devotional/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Devotional(models.Model):
+    "title month day body"
+    title = models.CharField(max_length="75")
+    month = models.IntegerField()
+    day = models.IntegerField()
+    body = models.TextField()
